5561da65d82209d37b9a831cb99610ac4e14499a
Anselmo José Tavares Ferreira Junior <anselmo.jtfj@gmail.com>
2022-08-09 19:32:48 -0300

    Implement deposit and authentication

--- bank/bank.go
+++ bank/bank.go
@@ -5,26 +5,22 @@ import (
 	"sync"
 )
 
-type User struct {
-	cpf      string
-	name     string
-	password string
-}
-
 type Account struct {
-	user    *User
 	balance float64
-	lock    *sync.Mutex
+	mu      *sync.Mutex
 }
 
-type Bank struct {
-	users    map[string]User
-	accounts []Account
+func NewAccount() *Account {
+	account := Account{
+		mu: &sync.Mutex{},
+	}
+
+	return &account
 }
 
 func (a *Account) Withdraw(value float64) (float64, error) {
-	a.lock.Lock()
-	defer a.lock.Unlock()
+	a.mu.Lock()
+	defer a.mu.Unlock()
 
 	if a.balance < value {
 		return -1, fmt.Errorf("not enough money")
@@ -34,30 +30,41 @@ func (a *Account) Withdraw(value float64) (float64, error) {
 }
 
 func (a *Account) Deposit(value float64) float64 {
-	a.lock.Lock()
-	defer a.lock.Unlock()
+	a.mu.Lock()
+	defer a.mu.Unlock()
 
 	a.balance += value
 	return a.balance
 }
 
-func NewAccount(user *User) *Account {
-	account := Account{
-		user: user,
-		lock: &sync.Mutex{},
-	}
-
-	return &account
+type User struct {
+	cpf      string
+	name     string
+	password string
+	Account  *Account
 }
 
-func NewUser(cpf string, name string, password string) *User {
+func NewUser(cpf string, name string, password string, acc *Account) *User {
 	user := User{
 		cpf:      cpf,
 		name:     name,
 		password: password,
+		Account:  acc,
 	}
 
 	return &user
+}
+
+func (u *User) Authenticate(pass string) error {
+	if u.password != pass {
+		return fmt.Errorf("wrong password")
+	}
+
+	return nil
+}
+
+type Bank struct {
+	users map[string]User
 }
 
 func NewBank() *Bank {
@@ -71,11 +78,10 @@ func (b *Bank) SignUp(cpf string, name string, password string) error {
 		return fmt.Errorf("CPF already taken")
 	}
 
-	user := NewUser(cpf, name, password)
-	acc := NewAccount(user)
+	acc := NewAccount()
+	user := NewUser(cpf, name, password, acc)
 
 	b.users[cpf] = *user
-	b.accounts = append(b.accounts, *acc)
 
 	return nil
 }
@@ -90,4 +96,13 @@ func (b *Bank) SignIn(cpf string, password string) error {
 	}
 
 	return nil
+}
+
+func (b *Bank) GetUser(cpf string) (*User, error) {
+	user, ok := b.users[cpf]
+	if !ok {
+		return nil, fmt.Errorf("user not found")
+	}
+
+	return &user, nil
 }
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 
@@ -15,6 +14,15 @@ type SignUpBody struct {
 	Password string `json:"password"`
 }
 
+type Deposit struct {
+	Value float64 `json:"value"`
+}
+
+type Credentials struct {
+	CPF      string `json:"cpf"`
+	Password string `json:"password"`
+}
+
 var (
 	jrBank = bank.NewBank()
 )
@@ -22,7 +30,8 @@ var (
 func SignUpHandler(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		fmt.Printf("could not read request body")
+		http.Error(w, "Could not read request body", http.StatusInternalServerError)
+		return
 	}
 
 	signUp := SignUpBody{}
@@ -30,8 +39,48 @@ func SignUpHandler(w http.ResponseWriter, r *http.Request) {
 	jrBank.SignUp(signUp.CPF, signUp.Name, signUp.Password)
 }
 
+func GetAuthenticatedUser(r *http.Request) (*bank.User, error) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	credentials := Credentials{}
+	json.Unmarshal(body, &credentials)
+	user, err := jrBank.GetUser(credentials.CPF)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := user.Authenticate(credentials.Password); err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func DepositHandler(w http.ResponseWriter, r *http.Request) {
+	user, err := GetAuthenticatedUser(r)
+	if err != nil {
+		http.Error(w, "User not authenticated", http.StatusUnauthorized)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Could not read request body", http.StatusInternalServerError)
+		return
+	}
+
+	deposit := Deposit{}
+	json.Unmarshal(body, &deposit)
+	user.Account.Deposit(deposit.Value)
+}
+
 func main() {
 	http.HandleFunc("/signup", SignUpHandler)
+
+	http.HandleFunc("/deposit", DepositHandler)
 
 	http.ListenAndServe(":8081", nil)
 }
